1980ab739d5fdc87d1e9c895da738e050ca81116
Brayan Esteves <brayan.esteves93@gmail.com>
2023-06-08 11:37:15 -0400

    Init Project [07.06.2023]

---
+++ logisticcompany-apirest-springboot/src/main/java/com/halconbit/logisticcompany/LogisticcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.halconbit.logisticcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticcompanyApplication.class, args);
+	}
+
+}
